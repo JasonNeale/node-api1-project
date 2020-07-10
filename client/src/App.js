7d6e963ce3e56f3b69cfef9e8d2c784fb0985084
Jason Neale <jg13.1980@gmail.com>
2020-07-09 20:30:43 -0400

    Completed some stretch

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,28 @@ import './assets/css/App.css'
 
 // Local imports
 import UserList from './components/UserList'
+import AddUser from './components/AddUser'
 
 
 const App = () => {
   const [ users, setUsers ] = useState([])
+  const [ stateChange, setStateChange ] = useState(false)
+
+  
 
   return (
-    <div className="App">
+    <div className="App wrapper">
+      <h2>User List</h2>
+      {
+        useEffect(() => {
+          
+        }, users)
+      }
       <UserList users={users} setUsers={setUsers} />
+      <hr />
+      <h2>Add User</h2>
+      <AddUser users={users} />
+      <hr />
     </div>
   )
 }
--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -9,9 +9,19 @@ const UserList = ( { users, setUsers } ) => {
       axios.get('http://localhost:5000/api/users')
       .then((res) => {
         setUsers(res.data)
+        console.log('[AXIOS RESPONSE: ', res.data)
       })
       .catch((err) => { console.log('[AXIOS ERROR: ', err) })
     }, users)
+
+
+    const deleteUser = (id) => {
+        axios.delete(`http://localhost:5000/api/users/${id}`)
+        .then((res) => {
+          console.log('[AXIOS RESPONSE: ', res.data)
+        })
+        .catch((err) => { console.log('[AXIOS ERROR: ', err) })
+    }
 
     return (
         <Table celled inverted selectable>
@@ -20,6 +30,7 @@ const UserList = ( { users, setUsers } ) => {
                     <Table.HeaderCell>ID</Table.HeaderCell>
                     <Table.HeaderCell>Name</Table.HeaderCell>
                     <Table.HeaderCell>Bio</Table.HeaderCell>
+                    <Table.HeaderCell>Actions</Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
@@ -28,6 +39,7 @@ const UserList = ( { users, setUsers } ) => {
                     <Table.Cell>{item.id}</Table.Cell>
                     <Table.Cell>{item.name}</Table.Cell>
                     <Table.Cell>{item.bio}</Table.Cell>
+                    <Table.Cell><i onBlur={deleteUser(item.id)} class="far fa-trash-alt"></i> <i class="far fa-edit"></i></Table.Cell>
                 </Table.Row>
             ))}
             </Table.Body>
